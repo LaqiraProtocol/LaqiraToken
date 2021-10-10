3b33741b79f289740f0b779b2c9372ac4d69d843
lightY <lighty8214@gmail.com>
2021-10-10 18:22:16 +0330

    BEP20Basic interface created

---
+++ contracts/BEP20Basic.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/**
+ * @title BEP20Basic
+ * @dev Simpler version of BEP20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+
+interface BEP20Basic {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+}
